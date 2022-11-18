31fd463d664ea6b9b46291b1cd7ae42fe25a804f
想走过亚洲的熊 <1693889638@qq.com>
2022-11-18 12:38:37 +0800

    后端接口编写完成

--- controller/dbServer.js
+++ controller/dbServer.js
@@ -1,2 +1,99 @@
 //操作数据库
-const db = require('../lib/db');
+const dbModel = require("../lib/db");
+
+// 新建walls函数
+exports.insertWall = async (req, res) => {
+  let { type, message, name, userId, moment, label, color, imgUrl } = req.body; //解构赋值
+  await dbModel.insertWall([type, message, name, userId, moment, label, color, imgUrl]).then((result) => {
+    res.send({
+      code: 200,
+      message: result,
+    });
+  });
+};
+// 新建反馈函数
+exports.insertFeedBack = async (req, res) => {
+  let { wallId, userId, type, moment } = req.body; //解构赋值
+  await dbModel.insertFeedBack([wallId, userId, type, moment]).then((result) => {
+    res.send({
+      code: 200,
+      message: result,
+    });
+  });
+};
+// 新建评论函数
+exports.insertComment = async (req, res) => {
+  let { wallId, userId, imgUrl, comment, name, moment } = req.body; //解构赋值
+  await dbModel.insertComment([wallId, userId, imgUrl, comment, name, moment]).then((result) => {
+    res.send({
+      code: 200,
+      message: result,
+    });
+  });
+};
+//删除墙留言，主表对应多条子表一起删除函数
+exports.deleteWall = async (req, res) => {
+  let data = req.body;
+  if (data.imgUrl) {
+    // 如果图片地址存在
+  }
+  await dbModel.deleteWall(data.id).then((result) => {
+    res.send({
+      code: 200,
+      message: result,
+    });
+  });
+};
+//删除反馈
+exports.deleteFeedBack = async (req, res) => {
+  let data = req.body;
+  await dbModel.deleteFeedBack(data.id).then((result) => {
+    res.send({
+      code: 200,
+      message: result,
+    });
+  });
+};
+// 删除评论
+exports.deleteComment = async (req, res) => {
+  let data = req.body;
+  await dbModel.deleteComment(data.id).then((result) => {
+    res.send({
+      code: 200,
+      message: result,
+    });
+  });
+};
+// 查询分页留言墙 type 0 留言 1 相片
+exports.findWallPage = async (req, res) => {
+  let { page, pageSize, type, label } = req.body;
+  await dbModel.findWallPage(page, pageSize, type, label).then(async (result) => {
+    for (let i = 0; i < result.length; i++) {
+      //查找相应wall的赞、举报、撤销数据
+      //喜欢
+      result[i].like = await dbModel.feedbackCount(result[i].id, 0);
+      //举报
+      result[i].report = await dbModel.feedbackCount(result[i].id, 1);
+      //要求撤诉
+      result[i].revoke = await dbModel.feedbackCount(result[i].id, 2);
+      //是否点赞
+      result[i].islike = await dbModel.likeCount(result[i].id, result[i].userId);
+      //评论数
+      result[i].comcount = await dbModel.commentCount(result[i].id);
+    }
+    res.send({
+      code: 200,
+      message: result,
+    });
+  });
+};
+//倒叙分页查墙的评论
+exports.findCommentPage = async (req, res) => {
+  let { page, pageSize, id } = req.body;
+  await dbModel.findCommentPage(page, pageSize, id).then((result) => {
+    res.send({
+      code: 200,
+      message: result,
+    });
+  });
+};
--- routers/index.js
+++ routers/index.js
@@ -1,10 +1,51 @@
 //index 路由文件
-const controller = require("../controller/dbServer");
+const controller = require("../controller/dbServer"); //控制器
 module.exports = function (app) {
   app.get("/", (req, res) => {
     res.send({
       code: 200,
       message: "后端服务启动成功~",
+    });
+  });
+  // 新建留言墙路由
+  app.post("/insertwall", (req, res) => {
+    controller.insertWall(req, res);
+  });
+  // 新建反馈路由
+  app.post("/insertfeedback", (req, res) => {
+    controller.insertFeedBack(req, res);
+  });
+  // 新建评论路由
+  app.post("/insertcomment", (req, res) => {
+    controller.insertComment(req, res);
+  });
+  //删除墙留言，主表对应多条子表一起删除函数
+  app.post("/deletewall", (req, res) => {
+    controller.deleteWall(req, res);
+  });
+  //删除反馈
+  app.post("/deletefeedfack", (req, res) => {
+    controller.deleteFeedBack(req, res);
+  });
+  // 删除评论
+  app.post("/deletecomment", (req, res) => {
+    controller.deleteComment(req, res);
+  });
+  // 查询分页留言墙 type 0 留言 1 相片
+  app.post("/findwallpage", (req, res) => {
+    controller.findWallPage(req, res);
+  });
+  //倒叙分页查墙的评论
+  app.post("/findcommentpage", (req, res) => {
+    controller.findCommentPage(req, res);
+  });
+
+  //用户进入进行ip登记
+  app.get("/signip", (req, res) => {
+    let ip = req.ip;
+    res.send({
+      code: 200,
+      ip: ip,
     });
   });
 };
